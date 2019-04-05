da590e404369c026ff047349cff064e1ab5ed4aa
Nico <28723269+ricewaffle@users.noreply.github.com>
2019-04-05 10:08:30 +0100

    Implemented use of cookies for authentication (get request parameter authentication is still present) resolves #7

--- src/me/nico/loginform/LoginServlet.java
+++ src/me/nico/loginform/LoginServlet.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +30,24 @@ public class LoginServlet extends HttpServlet
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		String cookieUsername = "";
+		String cookieToken = "";
+		
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null)
+		{
+			for (Cookie cookie : cookies)
+			{
+				if (cookie.getName().equals("username"))
+					cookieUsername = cookie.getValue();
+				else if (cookie.getName().equals("token"))
+					cookieToken = cookie.getValue();
+			}
+			
+			if (cookieUsername != "" && cookieToken != "")
+				response.sendRedirect("/LoginForm/MainPageServlet");
+		}
+		
 		PrintWriter out = response.getWriter();
 		
 		String[] html = HTMLReader.readHtml(GlobalInfo.PROJECT_PATH + "/WebContent/loginPage.html");
@@ -43,7 +62,14 @@ public class LoginServlet extends HttpServlet
 
 		boolean verified = GlobalInfo.userDatabase.verify(request.getParameter("usernameinput"), request.getParameter("passwordinput"));
 		if (verified)
-			response.sendRedirect("/LoginForm/MainPageServlet?username=" + request.getParameter("usernameinput") + "&token=" + SessionManager.generateToken(request.getParameter("usernameinput")));
+		{
+			String token = SessionManager.generateToken(request.getParameter("usernameinput"));
+			Cookie usernameCookie = new Cookie("username", request.getParameter("usernameinput"));
+			Cookie tokenCookie = new Cookie("token", token);
+			response.addCookie(usernameCookie);
+			response.addCookie(tokenCookie);
+			response.sendRedirect("/LoginForm/MainPageServlet?username=" + request.getParameter("usernameinput") + "&token=" + token);
+		}
 		else
 			out.print("<br><br>"
 					+ "<center>"
--- src/me/nico/loginform/MainPageServlet.java
+++ src/me/nico/loginform/MainPageServlet.java
@@ -5,10 +5,10 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 @WebServlet("/MainPageServlet")
 public class MainPageServlet extends HttpServlet
 {
@@ -22,39 +22,46 @@ public class MainPageServlet extends HttpServlet
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		PrintWriter out = response.getWriter();
-		String token = request.getParameter("token");
-		String username = request.getParameter("username");
+		
+		//[0]: isVerified, [1]: username, [2]: token, [3]: response
+		Object[] verificationCheck = verificationCheck(request, response);
+		
+		if (!(boolean) verificationCheck[0])
+		{
+			response.sendRedirect(request.getContextPath() + "/LoginServlet");
+			return;
+		}
+		
+		//response = (HttpServletResponse) verificationCheck[3];
+		
+		String username = (String) verificationCheck[1];
+		String token = (String) verificationCheck[2];
 		
 		String logoutTag = request.getParameter("logout");
 		
 		if (logoutTag != null && logoutTag.equals("true"))
 		{
-			SessionManager.unauthenticateToken(request.getParameter("username"), request.getParameter("token"));
+			SessionManager.unauthenticateToken(username, token);
 			response.sendRedirect(request.getContextPath() + "/LoginServlet");
 			return;
 		}
 		
-		if (token == null || username == null || !SessionManager.authenticateToken(username, token))
-			response.sendRedirect("LoginServlet");
-		else
-		{
-			String[] html = HTMLReader.readHtml(GlobalInfo.PROJECT_PATH + "/WebContent/mainPage.html");
-			String[] navigationBarHtml = HTMLReader.readHtml(GlobalInfo.PROJECT_PATH + "/WebContent/navigationBar.html");
-			String title = GlobalInfo.userDatabase.getGender(username).equals("Male") ? "Mr. " : "Mrs. ";
-			String name = GlobalInfo.userDatabase.getName(username);
-			
-			out.print(html[0] + request.getContextPath() + html[1] + navigationBarHtml[0] + request.getContextPath() +
-					navigationBarHtml[1] + html[2] + title + name + html[3]
-					+ username + html[4] + token + html[5]
-					+ username + html[6] + token + html[7]
-					+ username + html[8] + token + html[9]
-					+ username + html[10] + token + html[11]
-					+ username + html[12] + token + html[13]
-					+ username + html[14] + token + html[15]
-					+ username + html[16] + token + html[17]
-					+ username + html[18] + token + html[19]
-					+ username + html[20] + token + html[21]);
-		}
+		String[] html = HTMLReader.readHtml(GlobalInfo.PROJECT_PATH + "/WebContent/mainPage.html");
+		String[] navigationBarHtml = HTMLReader.readHtml(GlobalInfo.PROJECT_PATH + "/WebContent/navigationBar.html");
+		String title = GlobalInfo.userDatabase.getGender(username).equals("Male") ? "Mr. " : "Mrs. ";
+		String name = GlobalInfo.userDatabase.getName(username);
+	
+		out.print(html[0] + request.getContextPath() + html[1] + navigationBarHtml[0] + request.getContextPath() +
+				navigationBarHtml[1] + html[2] + title + name + html[3]
+				+ username + html[4] + token + html[5]
+				+ username + html[6] + token + html[7]
+				+ username + html[8] + token + html[9]
+				+ username + html[10] + token + html[11]
+				+ username + html[12] + token + html[13]
+				+ username + html[14] + token + html[15]
+				+ username + html[16] + token + html[17]
+				+ username + html[18] + token + html[19]
+				+ username + html[20] + token + html[21]);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -68,12 +75,63 @@ public class MainPageServlet extends HttpServlet
 			String type = request.getParameter("type");
 			
 			response.sendRedirect(request.getContextPath() + "/StockServlet?symbol=" + searchinput + "&type=" + type + "&username=" + username + "&token=" + token);
+			return;
 		} else {
 			String type = "volume";
 			String[] searchParams = navSearchInput.split(":");
 			if (searchParams.length > 1)
 				type = searchParams[1];
 			response.sendRedirect(request.getContextPath() + "/StockServlet?symbol=" + searchParams[0] + "&type=" + type + "&username=" + request.getParameter("username") + "&token=" + request.getParameter("token"));
+			return;
 		}
+	}
+	
+	
+	private Object[] verificationCheck(HttpServletRequest request, HttpServletResponse response)
+	{
+		String username = "";
+		String token = "";
+		
+		Cookie usernameCookie = null;
+		Cookie tokenCookie = null;
+		
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null)
+		{
+			for (Cookie cookie : cookies)
+			{
+				if (cookie.getName().equals("username"))
+				{
+					username = cookie.getValue();
+					usernameCookie = cookie;
+				}
+				else if (cookie.getName().equals("token"))
+				{
+					token = cookie.getValue();
+					tokenCookie = cookie;
+				}
+			}
+		
+			if (username != "" && token != "")
+			{
+				if (SessionManager.authenticateToken(username, token))
+					return new Object[] { true, username, token, response };
+				else
+				{
+					usernameCookie.setMaxAge(0);
+					tokenCookie.setMaxAge(0);
+					response.addCookie(usernameCookie);
+					response.addCookie(tokenCookie);
+					return new Object[] { false, username, token, response };
+				}
+			}
+		}
+		
+		username = request.getParameter("username");
+		token = request.getParameter("token");
+		
+		if (token == null || username == null || !SessionManager.authenticateToken(username, token))
+			return new Object[] { false, null, null, response };
+		return new Object[] { true, username, token, response };
 	}
 }
